bebd6c9d72f8251fb7c7ac6c1e885b074bd91daa
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2025-08-08 14:28:03 -0500

    Amazon_2025

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 def cargar_datos(ruta='../data/raw/amazon_sales_data 2025.csv'):
     return pd.read_csv(ruta)
 
-def guardar_datos(df, ruta='../data/processed/datos_eda.csv'):
+def guardar_datos(df, ruta='../data/processed/datos_limpios.csv'):
     df.to_csv(ruta, index=False)
